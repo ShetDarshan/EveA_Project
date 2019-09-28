5a38bcb503cc5a283a02d3bbc6f27b9215690fc9
Sharath19940604 <ghsharath6@gmail.com>
2019-09-28 20:22:20 +0100

    commiting front end changes

--- functions/index.js
+++ functions/index.js
@@ -26,22 +26,61 @@ admin.initializeApp({
 //   });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log("illi bartha idini")
-app.post("/register",(req,res) => {
-  var email=req.body.email;
-  var password =req.body.password;
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then(function(userRecord)
-  {
-    res.location('/user/' + userRecord.uid);
-    res.status(201).end();
-  })
-  .catch(function(error)
-  {
-    res.write
-    ({
-      code: error.code
-    });
-    res.status(401).end();
-  });
+// console.log("illi bartha idini")
+// app.post("/register",(req,res) => {
+//   var email=req.body.email;
+//   var password =req.body.password;
+//   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+ 
+//   })
+// })
+
+  app.post('/register', function(req, res, next) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    // Validation
+    // req.checkBody('username', 'Username is required').notEmpty();
+    // req.checkBody('email', 'Email is required').notEmpty();
+    // req.checkBody('email', 'Email is not valid').isEmail();
+    // req.checkBody('password', 'Password is required').notEmpty();
+    // req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    // var errors = req.validationErrors();
+
+    if(errors){
+        res.render('users/register', {
+            errors: errors
+        });
+    } else {
+        console.log("Started 1...");
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error, userData) {
+            console.log("Started 2...");
+            if(error){
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                req.flash('error_msg', 'Registration Failed. Make sure all fields are properly filled.' + error.message);
+                res.redirect('/users/register');
+                console.log("Error creating user: ", error);
+            } else {
+                console.log("Successfully created");
+                console.log("Successfully created user with uid:", userData.uid);
+                var user = { uid: userData.uid,
+                  email: email,
+                  username: username
+              }
+
+              var userRef = firebase.database().ref('users/');
+              userRef.push().set(user);
+
+              req.flash('success_msg', 'You are now registered and can login');
+              res.redirect('/users/login');
+          }
+
+      });
+  }
 });
---
+++ public/precache-manifest.48acf0cfca27639dd3aaf05c1ec95d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f3f692ad77f2bc9e9ed35512de221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652645b254dfb35a88d3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fd134f49e61df0ed0cd",
+    "url": "/static/js/2.8127df82.chunk.js"
+  },
+  {
+    "revision": "652645b254dfb35a88d3",
+    "url": "/static/js/main.a81f1f4d.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
