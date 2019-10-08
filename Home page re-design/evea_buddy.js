f266fb1c476827bbc417e10cbc3c86b793bd28bb
NetraJay <45148429+NetraJay@users.noreply.github.com>
2019-10-09 02:55:07 +0530

    Home Page
    Single page design to Sign Up and Login
    **Kindly add Facebook and google login features.
    
    evea_buddy includes card component of the Home page

---
+++ Home page re-design/evea_buddy.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
+class EveaBuddy extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div> 
+              <Card style={{width:'auto'}}>
+            <CardImg top width="100%" src='./images/event3.jpg' alt="Card image cap" />
+            <CardBody>
+              <CardTitle>Name</CardTitle>
+              <CardSubtitle>Palce</CardSubtitle>
+              <CardText>Animal</CardText>
+            </CardBody>
+          </Card></div>
+        );
+    }
+}
+
+export default EveaBuddy;
