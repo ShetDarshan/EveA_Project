2c94463ead847573704bf6cdd208af1d9cfba230
ShetDarshan <48444327+ShetDarshan@users.noreply.github.com>
2019-10-08 22:34:17 +0100

    commiting landing page and modal changes

---
+++ public/precache-manifest.f9d91d79bc6e2ebf0b2bb0e2fe06fd0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74316afa5617bec35d7127e83bf20520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a228db958d0ca95981",
+    "url": "/static/css/main.41090f5c.chunk.css"
+  },
+  {
+    "revision": "b30d1a9639ade15132dd",
+    "url": "/static/js/2.2ea1a946.chunk.js"
+  },
+  {
+    "revision": "40a228db958d0ca95981",
+    "url": "/static/js/main.bdf17c9e.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  },
+  {
+    "revision": "4d5926ecb6d38b45f6457d1f7ad97b6b",
+    "url": "/static/media/evea.4d5926ec.jpg"
+  }
+]);
