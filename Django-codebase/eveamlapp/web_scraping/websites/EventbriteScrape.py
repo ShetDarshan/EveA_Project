e226323c9eb7dbeb47b22d56aad21cd41c3276c7
Pooja-suvarna <55656717+Pooja-suvarna@users.noreply.github.com>
2019-10-15 17:32:33 +0100

    Revert "Integrating preprocessed eventbrite data to django"

--- Django-codebase/eveamlapp/web_scraping/websites/EventbriteScrape.py
+++ Django-codebase/eveamlapp/web_scraping/websites/EventbriteScrape.py
@@ -4,11 +4,8 @@ import requests
 import json
 import uuid
 import re
-from bs4 import BeautifulSoup as soup
-import datetime
 
 from eveamlapp.web_scraping.models import EventData
-
 
 class EventIe:
 
@@ -16,121 +13,77 @@ class EventIe:
     def scrape(urlOriginal):
 
         data_list = []
-        i = 0
-        for value in range(1, 67):
+        i=0
+        for value in range(1,20):
             url = ""
             url = urlOriginal+format(value)
             print(url)
             JSONContent = requests.get(url).json()
-            content = json.dumps(JSONContent, indent=4, sort_keys=True)
-            # print(content)
+            content = json.dumps(JSONContent, indent = 4, sort_keys= True)
+            #print(content)  
             data = json.loads(content)
-            # location and categories
-            # replacing category_id with their corresponding category name
-            # scraping category id and name
-            url1 = "https://www.eventbriteapi.com/v3/categories/?token=4KFS7BDPSZ5A5KWQ62KZ"
-            catJSONContent = requests.get(url1).json()
-            catcontent = json.dumps(catJSONContent, indent=4, sort_keys=True)
-            # print(content)
-            categorylist = json.loads(catcontent)
 
-            category_list = []
 
-            for categories in categorylist['categories']:
-                name = categories['name']
-                id_cat = categories['id']
-                categoryy = name, id_cat
-                category_list.append(categoryy)
-
-            category_list = list(category_list)
 
             for events in data['events']:
                 name = events['name']['text']
-                description = events['description']['text'].strip('\n')
+                description = events['description']['text']
                 description = format(description)
                 link = events['url']
                 start_datetime = events['start']['local']
-
-                # formating start date and time
-                start_date_split = start_datetime.split('T')
-                start_date = start_date_split[0]
-                start_time = start_date_split[1]
-                start_date = start_date.split('-')
-                date = start_date[2]
-                month = start_date[1]
-                year = start_date[0]
-                month = datetime.datetime.strptime(month,'%m').strftime('%B')
-                start_date = date + (' ')+ month+(' ')+ year
-        
-                #end_date
                 end_datetime = events['end']['local']
-
-                # formating end date and time
-                end_date_split = end_datetime.split('T')
-                end_date = end_date_split[0]
-                end_time = end_date_split[1]
-                end_date = end_date.split('-')
-                date = end_date[2]
-                month = end_date[1]
-                year = end_date[0]
-                month = datetime.datetime.strptime(month,'%m').strftime('%B')
-                end_date = date + (' ')+ month+(' ')+ year
-                #event price
-                free_event = events['is_free']
-                if free_event == True:
-                    price = 'free'
-                else:
-                    price = 'check link for more details'
+                #free_event = events['is_free']
                 category_id = events['category_id']
-                # replacing category_id with category name
-                for each in category_list:
-                    if category_id in each[1]:
-                        category = each[0]
-
-
                 try:
                     img = events['logo']['original']['url']
                 except:
                     img = 'none'
-                Location = events['venue']['address']['localized_multi_line_address_display']
-                #location formatting
-                location = str(Location).strip('[]')
-                location = location.split(',')
-                try:    
-                    location[0] = location[0].strip("''")
-                except:
-                    pass
                 try:
-                    location[1] = location[1].strip(" ''")
+                    Location = events['venue']['address']['localized_multi_line_address_display']
+                    Location = format(Location)
                 except:
-                    pass
-                #print(location[0])
-                #print(location[1])
-                try:
-                    location = location[0] + location[1]
-                except:
-                    location = location[0]
+                    Location = 'none'    
+                
+                # Category name fetching from Eventbrite
+                
+                
+                url = "https://www.eventbriteapi.com/v3/categories/?token=4KFS7BDPSZ5A5KWQ62KZ"
+
+                JSONContent = requests.get(url).json()
+                content = json.dumps(JSONContent, indent = 4, sort_keys= True)
+                categorylist = json.loads(content)
+                category_list = []
+
+                for categories in categorylist['categories']:
+                    name=categories['name']
+                    id_cat=categories['id']
+                    cate = {}
+                    cate["name"] = name
+                    cate["category_id"] = id_cat
         
+                    category_list.append(cate)
+
+
 
                 data = EventData()
 
                 data.id = uuid.uuid1().__str__()
                 data.title = name
-                data.time = start_time
-                data.location = location
+                data.time = ''
+                data.location = Location
                 data.summary = description
                 data.img = img
-                data.category = category
-                data.startdate = start_date
+                data.category = category_id
+                data.startdate = start_datetime
                 data.read_more = link
-                # data.address = address
-                data.enddate = end_date
-                data.price = price
+                #data.address = address
+                data.enddate = end_datetime
+                data.price = ''
                 data_list.append(data)
-                i = i+1
+                i=i+1
 
-            # print(len(data))
-
+            #print(len(data))
+    
         print(len(data_list))
 
         return data_list
