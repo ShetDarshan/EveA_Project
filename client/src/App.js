82b8d767534d4fbb52e15b82502352c29be267e3
NetraJay <45148429+NetraJay@users.noreply.github.com>
2019-10-16 14:27:49 +0530

    removed_scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import './App.scss';
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Register from './components/authentication/Register';
