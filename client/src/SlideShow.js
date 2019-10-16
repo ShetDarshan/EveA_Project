86ef52ab05221ffc65bd37fd83f8c15664719a31
NetraJay <45148429+NetraJay@users.noreply.github.com>
2019-10-17 00:37:22 +0530

    calendar

--- client/src/SlideShow.js
+++ client/src/SlideShow.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from './components/dashboard/Card';
 import data from './components/dashboard/data';
+
 import {Navbar, NavbarBrand} from 'react-bootstrap';
 import Example from './components/dashboard/navbar_toggler';
 import Search from  './components/dashboard/searchbox';
+
 
 
 // class component
@@ -26,7 +28,7 @@ class SlideShow extends Component {
     })
   }
 
-  prevProperty = () => {
+  prevProperty = () => {  
     const newIndex = this.state.property.index-1;
     this.setState({
       property: data.properties[newIndex]
@@ -42,7 +44,7 @@ class SlideShow extends Component {
               <Search/>
               <Example/>
               
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white", height:"50px"}}>
                 <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >UPCOMING EVENTS</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
@@ -72,8 +74,8 @@ class SlideShow extends Component {
                       </div>
                     </div>
               </div>
-             <div style={{paddingTop: "422px"}} id="free">
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
+             <div style={{paddingTop: "210px"}} id="free">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
                 <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >FREE EVENTS</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
@@ -83,7 +85,7 @@ class SlideShow extends Component {
                 </button>
                 &nbsp;&nbsp;
                 <button style={{ borderRadius: 30 }}
-                onClick={() => this.nextProperty()} 
+                onClick={() => this.nextProperty('free')} 
                 disabled={property.index === data.properties.length-1}
               >Next</button>
                       </ul>
@@ -104,9 +106,9 @@ class SlideShow extends Component {
                     </div>
               </div>
               </div>
-              <div style={{paddingTop: "422px"}} id="music">
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
-                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >MUSIC EVENTS</h4></NavbarBrand>
+              <div style={{paddingTop: "210px"}} id="music">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
+                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >MUSIC AND ENTERTAINMENT</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
                   onClick={() => this.prevProperty()} 
@@ -136,9 +138,9 @@ class SlideShow extends Component {
                     </div>
               </div>
               </div>
-              <div style={{paddingTop: "422px"}} id="artandtheatre">
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
-                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >ART AND THEATRE</h4></NavbarBrand>
+              <div style={{paddingTop: "210px"}} id="artandtheatre">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
+                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >FASHION, ART AND THEATRE</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
                   onClick={() => this.prevProperty()} 
@@ -168,9 +170,9 @@ class SlideShow extends Component {
                     </div>
               </div>
               </div>
-              <div style={{paddingTop: "422px"}} id="businessandeducation">
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
-                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >BUSINESS AND EDUCATION</h4></NavbarBrand>
+              <div style={{paddingTop: "210px"}} id="businessandeducation">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
+                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >EDUCATION, BUSINESS AND TECHNOLOGY</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
                   onClick={() => this.prevProperty()} 
@@ -200,9 +202,9 @@ class SlideShow extends Component {
                     </div>
               </div>
               </div>
-              <div style={{paddingTop: "422px"}} id="foodanddrinks">
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
-                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >FOOD AND DRINKS</h4></NavbarBrand>
+              <div style={{paddingTop: "210px"}} id="foodanddrink">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
+                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >FOOD AND DRINK</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
                   onClick={() => this.prevProperty()} 
@@ -232,9 +234,9 @@ class SlideShow extends Component {
                     </div>
               </div>
               </div>
-              <div style={{paddingTop: "422px"}}>
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
-                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >TRAVEL</h4></NavbarBrand>
+              <div style={{paddingTop: "210px"}} id="tourism">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
+                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >TOURISM AND SIGHTSEEING</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
                   onClick={() => this.prevProperty()} 
@@ -254,7 +256,7 @@ class SlideShow extends Component {
                       display: "list-item",
                       float: "left"}}
                     >
-                      <div id = "upcomming" className="cards-slider-wrapper" style={{
+                      <div id = "upcoming" className="cards-slider-wrapper" style={{
                         'transform':`translateX(-${property.index*(50/properties.length)}%)`
                       }}>
                         {
@@ -264,8 +266,8 @@ class SlideShow extends Component {
                     </div>
               </div>
               </div>
-              <div style={{paddingTop: "422px"}}>
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
+              <div style={{paddingTop: "210px"}} id="health">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
                 <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >HEALTH AND SPORTS</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
@@ -296,8 +298,8 @@ class SlideShow extends Component {
                     </div>
               </div>
               </div>
-              <div style={{paddingTop: "422px"}}>
-              <Navbar color="dark" light style={{background:"#af1a1a", color:"white"}}>
+              <div style={{paddingTop: "210px"}} id="community">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
                 <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >COMMUNITY AND FESTIVALS</h4></NavbarBrand>
                 <ul className="navbar-nav ml-auto">
                 <button style={{ borderRadius: 30 }}
@@ -307,6 +309,38 @@ class SlideShow extends Component {
                 </button>
                 &nbsp;&nbsp;
                 <button style={{ borderRadius: 30 }}
+                onClick={() => this.nextProperty()} 
+                disabled={property.index === data.properties.length-1}
+              >Next</button>
+                      </ul>
+                      </Navbar> 
+                <div> 
+                    <div className={`cards-slider active-slide-${property.index}`} 
+                    style={{
+                      display: "list-item",
+                      float: "left"}}
+                    >
+                      <div id = "upcoming" className="cards-slider-wrapper" style={{
+                        'transform':`translateX(-${property.index*(100/properties.length)}%)`
+                      }}>
+                        {
+                          properties.map(property => <Card key={property._id} property={property} />)
+                        }
+                      </div>
+                    </div>
+              </div>
+              </div>
+              <div style={{paddingTop: "210px"}} id="other">
+              <Navbar color="dark" light style={{background:"#af1a1a", color:"white",height:"50px"}}>
+                <NavbarBrand href="/upcomingevents" className="mr-auto"><h4 style={{color:"white"}} >OTHER</h4></NavbarBrand>
+                <ul className="navbar-nav ml-auto">
+                <button id="other" style={{ borderRadius: 30 }}
+                  onClick={() => this.prevProperty()} 
+                  disabled={property.index === 0}
+                >Prev
+                </button>
+                &nbsp;&nbsp;
+                <button id="ot" style={{ borderRadius: 30 }}
                 onClick={() => this.nextProperty()} 
                 disabled={property.index === data.properties.length-1}
               >Next</button>
@@ -329,13 +363,7 @@ class SlideShow extends Component {
               </div>
               </div>
             {/* <Search/> */}
-      </div>
-
-      
-
-
-
-      
+      </div>    
     );
   }
 }
