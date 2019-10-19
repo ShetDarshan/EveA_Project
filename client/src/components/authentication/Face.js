9cf352adcb8c9b0dc1b53f67719368a7f79a0d70
Sharath19940604 <ghsharath6@gmail.com>
2019-10-19 15:52:18 +0100

    commit

--- client/src/components/authentication/Face.js
+++ client/src/components/authentication/Face.js
@@ -15,7 +15,7 @@ const config = {
   // Configure FirebaseUI.
 const uiConfig = {
     // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
+    signInFlow: 'redirect',
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
     signInSuccessUrl: 'https://evea-prj.firebaseapp.com/eventboard',
     // We will display Google and Facebook as auth providers.
