e3b1d23ab594ac169de07d43ae21271f1d5332f7
swathimahesh <d18123606@mytudublin.ie>
2019-11-06 23:17:07 +0000

    main

---
+++ client/src/components/eventboard/MainSlider.js
@@ -0,0 +1,48 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../../css/App.css";
+import React from "react";
+import EventCategories from "../eventCategories/EventCategories";
+import Slider from "react-slick";
+export default  class UpcomingEvents extends React.Component {
+    
+    render() {
+      var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 5000
+      };
+      return (
+        <div className="jumbotron slider">
+            <div className="mainSlider">
+                    <Slider {...settings}>
+                    <div>
+                        <img src={require('./../../img/music.jpg')} alt="Bacn"></img>
+                    </div>
+                    <div>
+                        <img src={require('./../../img/sports.jpg')} alt="Bacn"></img>
+                    </div>
+                    <div>
+                        <img src={require('./../../img/music.jpg')}  alt="Bacn"></img>
+                    </div>
+                    <div>
+                        <img src={require('./../../img/music.jpg')} alt="Bacn"></img>
+                    </div>
+                    <div>
+                        <img src={require('./../../img/sports.jpg')} alt="Bacn"></img>
+                    </div>
+                    <div>
+                        <img src={require('./../../img/music.jpg')} alt="Bacn"></img>
+                    </div>
+                    </Slider>
+            </div>
+            <EventCategories/>
+      </div>
+        
+      );
+    }
+  }
