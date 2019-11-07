f5b756d9907464637a55072ca8a8cf2e7227b179
Sharath19940604 <ghsharath6@gmail.com>
2019-11-07 01:16:36 +0000

    commit

--- client/src/components/eventCategories/EventDetails.js
+++ client/src/components/eventCategories/EventDetails.js
@@ -6,16 +6,20 @@ import { getEventDetails,getRecmdEvents } from '../../actions/eventActions';
 
 
 class EventDetails extends Component {
-  componentDidMount() {
-    if (this.props.match.params.title) {
-      this.props.getEventDetails(this.props.match.params.title);
-    }
-    if ( this.props.match.params.title){
-        this.props.getRecmdEvents(this.props.match.params.title);
-        console.log("entering here")
-    }
-  }
-
+//   componentDidMount() {
+//     if (this.props.match.params.title) {
+//       this.props.getEventDetails(this.props.match.params.title);
+//     }
+//     if ( this.props.match.params.title){
+//         this.props.getRecmdEvents(this.props.match.params.title);
+//         console.log("entering here")
+//     }
+//   }
+constructor(props) {
+    super(props);
+    this.props.getEventDetails(this.props.match.params.title);
+    this.props.getRecmdEvents(this.props.match.params.title);
+}
   render() {
     const { eventDetails, loading,recom } = this.props.eventDetails;
     console.log(recom,"recom")
