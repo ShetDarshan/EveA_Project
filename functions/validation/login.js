68b949f7abfe94a9a336669b2357b1586dd33756
NetraJay <45148429+NetraJay@users.noreply.github.com>
2019-11-07 08:59:57 +0530

    check

--- functions/validation/login.js
+++ functions/validation/login.js
@@ -2,7 +2,7 @@ const isEmpty = require("is-empty");
 module.exports= function validateLoginData (data) {
     let errors = {};
   
-    if (isEmpty(data.email)) errors.email = 'Email Must not be empty';
+    if (isEmpty(data.email)) errors.email = ' not be empty';
     if (isEmpty(data.password)) errors.password = 'Password Must not be empty';
   
     return {
