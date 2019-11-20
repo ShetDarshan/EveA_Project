a304f4b02f5b2b2fb4e54b6baa7141efa04ef12e
ShetDarshan <d18123594@mytudublin.ie>
2019-11-20 20:37:40 +0000

    Friends Profile

--- client/src/components/eventCategories/EventCategories.js
+++ client/src/components/eventCategories/EventCategories.js
@@ -11,6 +11,21 @@ import { bool } from "prop-types";
  class EventCategories extends Component {
   componentDidMount() {
     console.log("Loading Component Mounted");
+    navigator.geolocation.getCurrentPosition(function (location) {
+      this.setState({
+        lat: location.coords.latitude,
+        lon: location.coords.longitude 
+      })
+    }.bind(this));
+
+    // OR
+    navigator.geolocation.getCurrentPosition(location => {
+      this.setState({
+        lat: location.coords.latitude,
+        lon: location.coords.longitude
+      })
+    });
+
   }
   constructor(props) {
     super(props);
@@ -18,14 +33,14 @@ import { bool } from "prop-types";
     props.getEvents();
   }
    render() {
-    let showItems = 4
+    let showItems = 4;
+    console.log("State",this.props.locationData);
+        if(window.innerWidth <= 576)showItems=1
+        else if(window.innerWidth <= 768)showItems=2
+        else if(window.innerWidth <= 1024)showItems=3
+        else showItems=4
 
-    if(window.innerWidth <= 576)showItems=1
-    else if(window.innerWidth <= 768)showItems=2
-    else if(window.innerWidth <= 1024)showItems=3
-    else showItems=4
-
-      const dataset  = this.props.events.events;
+    const dataset  = this.props.events.events;
      if (Object.keys(dataset).length < 1 ){
       console.log(" %c Loading the data from ajax" ,"background-color:#fff; color :#000;");
        return <div>Loading...</div>
@@ -45,26 +60,23 @@ import { bool } from "prop-types";
                   <div key={categoriesList+"-carousel"} className="carousel">     
                       <h4 key={categoriesList+"-heading"} className="text-capitalise">{categoriesList.toLowerCase()}</h4>
                       <div >
-                      <div key={categoriesList+"-container"} className="card text-white  mb-3 card-slider"  >
+                      <div key={categoriesList+"-container"} className="card text-white  mb-3 card-slider">
                       <Slider {...settings}>
                       {
-                          
-                            dataset[categoriesList].map(data => (
-                                    <div key={data.title+"card-slider"} className="card card-slider "  title= {data.title}>
-                                          <div key={data.title+"-body"} className="card-body"  > 
-                                          <div key={data.title+"-image-container"} className="imageContainer" >
-                                            <div key={data.title+"-background"} className="imageBg" style={{backgroundImage: `url(${data.img})`}}></div>
-                                          </div>
-                                          <Link to={`/event/${data.title}`} className="card-link">
-                                            <h6 key={data.title+"-desc"} title= {data.title} className="card-title mb-2 mt-2 pt-0 " style={{paddingTop:"50px"}}>{data.title}</h6>
-                                            </Link>
-                                            <h6 key={data.startdate+"-startdate"} className="card-subtitle mb-2 mt-2 pt-0"><b>Date: </b>{data.startdate}</h6>
-                                            <Link to={`/event/${data.title}`} className="card-link">
-                                                   View Event
-                                           </Link>
-                                            <a href={"https://maps.google.com/?q="+ data.latitude +","+ data.longitude } target="_blank" className="card-link">Show Route</a>
-                                          </div>
-                                        </div>  
+                        dataset[categoriesList].map(data => (
+                          <div key={data.title+"card-slider"} className="card card-slider "  title= {data.title}>
+                                <div key={data.title+"-body"} className="card-body"  > 
+                                <div key={data.title+"-image-container"} className="imageContainer" >
+                                  <div key={data.title+"-background"} className="imageBg" style={{backgroundImage: `url(${data.img})`}}></div>
+                                </div>
+                                  <Link to={`/event/${data.title}`} className="card-link">
+                                    <h6 key={data.title+"-desc"} title= {data.title} className="card-title mb-2 mt-2 pt-0 " style={{paddingTop:"50px"}}>{data.title}</h6>
+                                  </Link>
+                                  <h6 key={data.startdate+"-startdate"} className="card-subtitle mb-2 mt-2 pt-0"><b>Date: </b>{data.startdate}</h6>
+                                  <Link to={`/event/${data.title}`} className="card-link">View Event</Link>
+                                  <a href={`https://www.google.com/maps?saddr=${this.props.locationData.lat},${this.props.locationData.lon}&daddr=${data.latitude},${data.longitude}`} target="_blank" className="card-link">Show Route</a>
+                                </div>
+                              </div>  
                         ))}
                         </Slider>
                         </div>  
