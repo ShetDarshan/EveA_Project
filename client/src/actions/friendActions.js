25d9fbe39c657fdec622883f262f444857d9e86b
ShetDarshan <48444327+ShetDarshan@users.noreply.github.com>
2019-11-21 21:24:41 +0000

    adding the actions for friends going

--- client/src/actions/friendActions.js
+++ client/src/actions/friendActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SEND_REQ,GET_REQ ,ACC_REQ,REJ_REQ ,GOING_REQ ,N_GOING_REQ} from './types';
+import { SEND_REQ,GET_REQ ,ACC_REQ,REJ_REQ ,GOING_REQ ,N_GOING_REQ,FRNDS_GOING} from './types';
 
 export const sendFriendRequest = (friend) => dispatch => {
     // dispatch({ type: LOADING_DATA });
@@ -106,6 +106,25 @@ export const sendFriendRequest = (friend) => dispatch => {
           .catch(err =>
             dispatch({
               type: N_GOING_REQ,
+              payload: null
+            })
+          );
+      }; 
+
+      export const friendsGoing = (friend) => dispatch => {
+        // dispatch({ type: LOADING_DATA });
+        console.log("friends Going:",friend);
+        axios
+          .post('/api/v1/friendsGoing',friend)   
+          .then(res => 
+            dispatch({
+              type: FRNDS_GOING,
+              payload: res.data
+            })
+          )
+          .catch(err =>
+            dispatch({
+              type: FRNDS_GOING,
               payload: null
             })
           );
