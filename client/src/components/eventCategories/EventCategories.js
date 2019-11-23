b7b6e51a08602779af82880cba9a39f23f5461e0
NetraJay <45148429+NetraJay@users.noreply.github.com>
2019-11-23 13:51:11 +0530

    slideshow

--- client/src/components/eventCategories/EventCategories.js
+++ client/src/components/eventCategories/EventCategories.js
@@ -101,30 +101,45 @@ import Car4 from '../../img/Carousel5.PNG';
                {Object.keys(dataset).map(categoriesList => (
                   <div key={categoriesList+"-carousel"} className="carousel">  
                   <button onClick={()=>this.operation()}>Browse</button>   
-                      <h4 key={categoriesList+"-heading"} className="text-capitalise">{categoriesList.toLowerCase()}</h4>
+                      <h6 key={categoriesList+"-heading"} className="text-capitalise">{categoriesList.toLowerCase()}</h6>
                       {
                               this.state.showMe?
-
-                      <div key={categoriesList+"-container"} className="card text-white  mb-3 card-slider">
                       <Slider {...settings}>
                       {
                         dataset[categoriesList].map(data => (
                           <div key={data.title+"card-slider"} className="card card-slider "  title= {data.title}>
                                 <div key={data.title+"-body"} className="card-body"  > 
+
+
+
                                 <div key={data.title+"-image-container"} className="imageContainer" >
-                                  <div key={data.title+"-background"} className="imageBg" style={{backgroundImage: `url(${data.img})`}}></div>
+                                  <div key={data.title+"-background"} className="imageBg" style={{backgroundImage: `url(${data.img})`}}>
                                 </div>
-                                  <Link to={`/event/${data.title}`} className="card-link">
-                                    <h6 key={data.title+"-desc"} title= {data.title} className="card-title mb-2 mt-2 pt-0 " style={{paddingTop:"50px"}}>{data.title}</h6>
-                                  </Link>
+                                </div>
+
+
+
+
+<Link to={`/event/${data.title}`} className="card-link">
+<h6 key={data.title+"-desc"} title= {data.title} className="card-title mb-2 mt-2 pt-0 " style={{paddingTop:"50px"}}>{data.title}</h6>
+</Link>
+
+<div class="card-stats">
+      <div class="value">89</div>
+      <div class="type">Interested</div>
+    </div>
+    <div class="stat">
+      <div class="value">32</div>
+      <div class="type">Going</div>
+      </div>
+
                                   <h6 key={data.startdate+"-startdate"} className="card-subtitle mb-2 mt-2 pt-0">{data.startdate}</h6>
-                                  {/*<Link to={`/event/${data.title}`} className="card-link">View Event</Link>*/}
                                   <a href={`https://www.google.com/maps?saddr=${this.state.lat},${this.state.lon}&daddr=${data.latitude},${data.longitude}`} target="_blank" className="card-link">Show Route</a>
                                 </div>
                               </div>  
                         ))}
                         </Slider>
-                        </div>
+                        
                         :null
                       }
                   </div>
