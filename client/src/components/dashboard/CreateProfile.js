7ea7c8f9d87716ab345d4c19782b6d30dad5bad1
ShetDarshan <48444327+ShetDarshan@users.noreply.github.com>
2019-12-02 21:52:02 +0000

    friend changes

--- client/src/components/dashboard/CreateProfile.js
+++ client/src/components/dashboard/CreateProfile.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { getAllProfiles, getProfile, updateProfile, getSuggestedFrds } from '../../actions/profileActions';
-import { getfriendRequestList,acceptFriendRequest,rejectFriendRequest } from '../../actions/friendActions';
+import { getfriendRequestList, acceptFriendRequest, rejectFriendRequest } from '../../actions/friendActions';
 
 import "../../css/profile.css";
 import firebase from 'firebase';
 import noPic from '../../img/noPic.jpg';
 import { isNull } from 'util';
 import { Link } from 'react-router-dom';
-import { Button,Snackbar,SnackbarContent } from '@material-ui/core';
+import { Button, Snackbar, SnackbarContent } from '@material-ui/core';
 import card from "../../img/cover.png";
 import user from "../../img/user.png";
 
@@ -19,9 +19,9 @@ class CreateProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedUser :'',
-      requestedUser : '',
-      msg:false
+      loggedUser: '',
+      requestedUser: '',
+      msg: false
     };
     const { user } = this.props.auth;
     const profile = this.props.getProfile(user.email);
@@ -35,21 +35,21 @@ class CreateProfile extends Component {
     console.log("user", user);
     this.props.getProfile(user.email);
   }
-  acceptRequest = () => {
-    this.props.acceptFriendRequest(this.state)
-  }
-  rejectRequest = () => {
-    this.props.rejectFriendRequest(this.state)
-  }
+  // acceptRequest = () => {
+  //   this.props.acceptFriendRequest(this.state)
+  // }
+  // rejectRequest = () => {
+  //   this.props.rejectFriendRequest(this.state)
+  // }
   render() {
-    const { profile, profiles,sugessted } = this.props.users;
+    const { profile, profiles, sugessted } = this.props.users;
     // var { request  } =  this.props.friends;
-    console.log("suggested",sugessted);
+    console.log("suggested", sugessted);
     let allRequests = [];
-    if(this.props.friends.request.from ){
-      allRequests = this.props.friends.request.from;  
+    if (this.props.friends.request.from) {
+      allRequests = this.props.friends.request.from;
     }
-    
+
     // console.log("this.props.friends.request.from",this.props.friends.request.from);
     // console.log("console request", allRequests);
 
@@ -71,203 +71,205 @@ class CreateProfile extends Component {
 
     }
     return (
-     
-     
-          <div className="main-content-container container-fluid px-4">
-            
-              <div className="row mt-4">
-                
-                  <div className="col-sm-12 col-lg-4">
-                   <div className="card card-small user-details mb-4">
-                     <div className="card-header p-0">
-                       <div className="user-details_bg">
-                         <img src={card} /></div> 
-                         </div>
-                        <div className="card-body p-0">
-                    <div className="user-details__avatar mx-auto">
-                       {/* <div className="avtarImg" style={{ backgroundImage: `url(${userImageUrl})` }}></div> */}
-                       <img src={user}></img> 
+
+
+      <div className="main-content-container container-fluid px-4">
+
+        <div className="row mt-4">
+
+          <div className="col-sm-12 col-lg-4">
+            <div className="card card-small user-details mb-4">
+              <div className="card-header p-0">
+                <div className="user-details_bg">
+                  <img src={card} /></div>
+              </div>
+              <div className="card-body p-0">
+                <div className="user-details__avatar mx-auto">
+                  {/* <div className="avtarImg" style={{ backgroundImage: `url(${userImageUrl})` }}></div> */}
+                  <img src={user}></img>
+                </div>
+
+                <h4 class="text-center m-0 mt-2">{userName}</h4>
+                <p class="text-center text-light m-0 mb-2" style={{ color: "black" }}>{userBio}</p>
+
+                <div class="user-details__user-data border-top border-bottom p-4">
+                  <div class="row mb-3">
+                    <div class="col w-50">
+                      <span>Email</span>
+                      <span>sierra@example.com</span>
                     </div>
-                   
-                    <h4 class="text-center m-0 mt-2">{userName}</h4>
-        <p class="text-center text-light m-0 mb-2" style={{color:"black"}}>{userBio}</p>
-        
-        <div class="user-details__user-data border-top border-bottom p-4">
-          <div class="row mb-3">
-            <div class="col w-50">
-              <span>Email</span>
-              <span>sierra@example.com</span>
+                    <div class="col w-50">
+                      <span>Location</span>
+                      <span>Remote</span>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col w-50">
+                      <span>Phone</span>
+                      <span>+40 1234 567 890</span>
+                    </div>
+                    <div class="col w-50">
+                      <span>Account Number</span>
+                      <span>123456789</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="user-details__tags p-4" >
+                  <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{ color: "black" }}>User Experience</span>
+                  <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{ color: "black" }}>UI Design</span>
+                  <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{ color: "black" }}>React JS</span>
+                  <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{ color: "black" }}>HTML &amp; CSS</span>
+                  <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{ color: "black" }}>JavaScript</span>
+                  <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{ color: "black" }}>Bootstrap 4</span>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+              </div>
+
             </div>
-            <div class="col w-50">
-              <span>Location</span>
-              <span>Remote</span>
-            </div>
+
           </div>
-          <div class="row">
-            <div class="col w-50">
-              <span>Phone</span>
-              <span>+40 1234 567 890</span>
-            </div>
-            <div class="col w-50">
-              <span>Account Number</span>
-              <span>123456789</span>
-            </div>
+
+          <div className="w-25">
+            <h4 className="text-primary">Requests Received</h4>
+            <ul className="friendRequestList ">
+              {
+                allRequests && allRequests.map(data => {
+                  return (
+                    <li className="m-2 card border-primary ">
+                      <div className=""><div class="friendAvtar text-center">
+                        <div className="avtarImg" style={{ backgroundImage: `url(https://i1.sndcdn.com/avatars-000316300368-x3f9sd-t500x500.jpg)` }}></div>
+                        <Link to={`/friend/${data}`}>{data}</Link>
+                        <button className="btn btn-sm btn-info btn-sm mr-2"
+                          onClick={() => {
+                            this.setState({
+                              msg: true
+                            })
+                            const request = ({
+                              loggedUser: userEmail,
+                              requestedUser: data
+                            })
+                            this.props.acceptFriendRequest(request)
+                          }}> Accept Request</button>
+                        <button className="btn btn-sm btn-primary btn-sm"
+                          onClick={() => {
+                            const request = ({
+                              loggedUser: userEmail,
+                              requestedUser: data
+                            })
+                            this.props.rejectFriendRequest(request)
+                          }}> Reject Request</button>
+                      </div>
+                      </div>
+                    </li>
+                  )
+                })
+              }
+            </ul>
           </div>
         </div>
-        <div class="user-details__tags p-4" >
-          <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{color:"black"}}>User Experience</span>
-          <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{color:"black"}}>UI Design</span>
-          <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{color:"black"}}>React JS</span>
-          <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{color:"black"}}>HTML &amp; CSS</span>
-          <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{color:"black"}}>JavaScript</span>
-          <span class="badge badge-pill badge-light text-light text-uppercase mb-2 border" style={{color:"black"}}>Bootstrap 4</span>
+        <div className="row m-4">
+          <h6 className="w-100"><span className="text-muted mr-2">Interests:</span> <b className="bold">{userInterests}</b> </h6>
+          <h6 className="w-100"><span className="text-muted mr-2">Address:</span><b className="bold">{userAddress}</b> </h6>
+          <h6 className="w-100"><span className="text-muted mr-2">Date Of Birth:</span> <b className="bold">{userBirthday}</b></h6>
+          <h6 className="w-100"><span className="text-muted mr-2">Email:</span> <b className="bold">{userEmail}</b></h6>
         </div>
-
-
-
-
-
-
-
-
-
-
-                   
-                     
-                    </div>
-                   
-                   </div>
-                  
-                  </div>    
-                        
-                <div className="w-25">
-                <h4 className="text-primary">Requests Received</h4>
-                  <ul className="friendRequestList ">
-                    {
-                      allRequests && allRequests.map(data => {
-                        return (
-                          <li className="m-2 card border-primary ">
-                            <div className=""><div class="friendAvtar text-center">
-                              <div className="avtarImg" style={{ backgroundImage: `url(https://i1.sndcdn.com/avatars-000316300368-x3f9sd-t500x500.jpg)` }}></div>
-                              <Link  to={`/friend/${data}`}>{data}</Link>
-                              <button className="btn btn-sm btn-info btn-sm mr-2" 
-                              onClick={() => {
-                                this.setState({
-                                  msg:true
-                                })
-                                const request = ({
-                                  loggedUser: userEmail,
-                                  requestedUser: data
-                                })
-                                this.acceptRequest(request)
-                            }}> Accept Request</button>
-                              <button className="btn btn-sm btn-primary btn-sm"
-                              onClick={() => {
-                                const request = ({
-                                  loggedUser: userEmail,
-                                  requestedUser: data
-                                })
-                                this.rejectRequest(request)
-                            }}> Reject Request</button>
-                            </div>
-                            </div>
-                          </li>
-                        )
-                      })
-                    }
-                  </ul>
-                </div> 
-              </div>
-              <div className="row m-4">
-                <h6 className="w-100"><span className="text-muted mr-2">Interests:</span> <b className="bold">{userInterests}</b> </h6>
-                <h6 className="w-100"><span className="text-muted mr-2">Address:</span><b className="bold">{userAddress}</b> </h6>
-                <h6 className="w-100"><span className="text-muted mr-2">Date Of Birth:</span> <b className="bold">{userBirthday}</b></h6>
-                <h6 className="w-100"><span className="text-muted mr-2">Email:</span> <b className="bold">{userEmail}</b></h6>
-              </div>
-              {/* <div className="row">
+        {/* <div className="row">
                 <div className="col-md-12 ml-auto mr-auto">
                 </div>
               </div> */}
-              <Link className="nav-link" to="/searchUsers">Search Users</Link>
-              <div className="row">
-                <div className="col-md-12 ml-auto mr-auto">
-                  <h4 className="text-primary">Suggested Friends</h4>
-                  <ul className="customFriendList">
-                    {
+        <Link className="nav-link" to="/searchUsers">Search Users</Link>
+        <div className="row">
+          <div className="col-md-12 ml-auto mr-auto">
+            <h4 className="text-primary">Suggested Friends</h4>
+            <ul className="customFriendList">
+              {
 
-// sugessted && sugessted.map((e,i )=>{ e.map((f,j)=> {console.log(f.email,"final")})}) 
+                // sugessted && sugessted.map((e,i )=>{ e.map((f,j)=> {console.log(f.email,"final")})}) 
 
-sugessted && sugessted.map((data,i) =>{ return(data.map((f,j)=> {
-  return (
+                sugessted && sugessted.map((data, i) => {
+                  return (data.map((f, j) => {
+                    return (
 
-    <li key={"profile-"+f} className="card border-primary m-2 p-2">
-      <Link key={"profile-link-"+f.handle} to={`/friend/${f.email}`}>
-          <div key={"profile-container-"+f.handle} className="text-center">
-            <div key={"profile-friendAvtar-"+f.handle} className="friendAvtar m-auto">
-              <div key={"profile-background-"+f.handle} className="avtarImg" style={{ backgroundImage: `url(${f.imageUrl})` }}></div>
-            </div>
-          </div>
-          <h5 key={"profile-handle-"+f.handle} className="mt-2 text-primary text-center font-weight-bold">{f.handle}</h5>
-      </Link>
-    </li>
-  )
-}
-  ) )} ) 
+                      <li key={"profile-" + f} className="card border-primary m-2 p-2">
+                        <Link key={"profile-link-" + f.handle} to={`/friend/${f.email}`}>
+                          <div key={"profile-container-" + f.handle} className="text-center">
+                            <div key={"profile-friendAvtar-" + f.handle} className="friendAvtar m-auto">
+                              <div key={"profile-background-" + f.handle} className="avtarImg" style={{ backgroundImage: `url(${f.imageUrl})` }}></div>
+                            </div>
+                          </div>
+                          <h5 key={"profile-handle-" + f.handle} className="mt-2 text-primary text-center font-weight-bold">{f.handle}</h5>
+                        </Link>
+                      </li>
+                    )
+                  }
+                  ))
+                })
 
-             
-                                 /* {
-                      sugessted && sugessted.map(data,i => {
-                      
-                        return (
 
-                          <li key={"profile-"+data} className="card border-primary m-2 p-2">
-                            <Link key={"profile-link-"+data.handle} to={`/friend/${data[i].email}`}>
-                                <div key={"profile-container-"+data[i].handle} className="text-center">
-                                  <div key={"profile-friendAvtar-"+data[i].handle} className="friendAvtar m-auto">
-                                    <div key={"profile-background-"+data[i].handle} className="avtarImg" style={{ backgroundImage: `url(${data[i].imageUrl})` }}></div>
-                                  </div>
-                                </div>
-                                <h5 key={"profile-handle-"+data[i].handle} className="mt-2 text-primary text-center font-weight-bold">{data[i].handle}</h5>
-                            </Link>
-                          </li>
-                        )
-                      })} */
-                    }
-
-                  </ul>
-                </div>
-                <Snackbar
-                        anchorOrigin={{
-                            vertical: "top",
-                            horizontal: "right"
-                        }}
-                        autoHideDuration={3000}
-                        open={this.state.msg}
-                        onClose={() => {
-                            this.setState({
-                                msg: false
-                            });
-                        }}
-                    >
-                        <SnackbarContent
-                            style={{
-                                backgroundColor: this.state.msg
-                                    ? "green"
-                                    : ""
-                            }}
-                            message={
-                                this.state.msg
-                                    ? "You Are Now Friends"
-                                    : ""
-                            }
-                        />
-                    </Snackbar>
-              </div>
-            
-          </div>
-        
+                /* {
+     sugessted && sugessted.map(data,i => {
      
-      
+       return (
+
+         <li key={"profile-"+data} className="card border-primary m-2 p-2">
+           <Link key={"profile-link-"+data.handle} to={`/friend/${data[i].email}`}>
+               <div key={"profile-container-"+data[i].handle} className="text-center">
+                 <div key={"profile-friendAvtar-"+data[i].handle} className="friendAvtar m-auto">
+                   <div key={"profile-background-"+data[i].handle} className="avtarImg" style={{ backgroundImage: `url(${data[i].imageUrl})` }}></div>
+                 </div>
+               </div>
+               <h5 key={"profile-handle-"+data[i].handle} className="mt-2 text-primary text-center font-weight-bold">{data[i].handle}</h5>
+           </Link>
+         </li>
+       )
+     })} */
+              }
+
+            </ul>
+          </div>
+          <Snackbar
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right"
+            }}
+            autoHideDuration={3000}
+            open={this.state.msg}
+            onClose={() => {
+              this.setState({
+                msg: false
+              });
+            }}
+          >
+            <SnackbarContent
+              style={{
+                backgroundColor: this.state.msg
+                  ? "green"
+                  : ""
+              }}
+              message={
+                this.state.msg
+                  ? "You Are Now Friends"
+                  : ""
+              }
+            />
+          </Snackbar>
+        </div>
+
+      </div>
+
+
+
     )
   }
 }
@@ -278,4 +280,4 @@ const mapStateToProps = state => ({
   friends: state.friends
 
 })
-export default connect(mapStateToProps, { getAllProfiles, getProfile, getfriendRequestList,rejectFriendRequest,acceptFriendRequest,getSuggestedFrds })(CreateProfile);
+export default connect(mapStateToProps, { getAllProfiles, getProfile, getfriendRequestList, rejectFriendRequest, acceptFriendRequest, getSuggestedFrds })(CreateProfile);
