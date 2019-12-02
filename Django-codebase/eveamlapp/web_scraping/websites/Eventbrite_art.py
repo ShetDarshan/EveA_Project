302630ff074e790fe6b00ad747722a5c4e267cb2
Swathi <D18123606@mytudublin.ie>
2019-12-02 03:14:07 +0000

    Changes to scrape files for updation of ordinates

--- Django-codebase/eveamlapp/web_scraping/websites/Eventbrite_art.py
+++ Django-codebase/eveamlapp/web_scraping/websites/Eventbrite_art.py
@@ -4,31 +4,18 @@ from bs4 import BeautifulSoup as soup
 import uuid
 import re
 import datetime
-<<<<<<< HEAD
 from .GetMonth import month_string_to_number
 from .geocoding_check import getOrdinates
-=======
-
->>>>>>> master
 from eveamlapp.web_scraping.models import EventData
 
 class EventbriteArt:
 
     @staticmethod
-<<<<<<< HEAD
     def scrape(urlOriginal,data_list):
   
         i = 0
         #5
         for value in range(1,2):
-=======
-    def scrape(urlOriginal):
-
-        data_list = []
-       
-        i = 0
-        for value in range(1,13):
->>>>>>> master
             url = ""
             url = urlOriginal+format(value)
             print(url)
@@ -48,17 +35,10 @@ class EventbriteArt:
                     Date_time = container.findAll('div',class_='eds-text-color--primary-brand eds-l-pad-bot-1 eds-text-weight--heavy eds-text-bs')[0].text
                 except:
                     Date_time='None'
-<<<<<<< HEAD
                 # try:
                 #     Location = container.findAll('div',class_='card-text--truncated__one')[0].text
                 # except:
                 #     Location='None'
-=======
-                try:
-                    Location = container.findAll('div',class_='card-text--truncated__one')[0].text
-                except:
-                    Location='None'
->>>>>>> master
                 try:
                     Price = container.findAll('div',class_='eds-media-card-content__sub eds-text-bm eds-text-color--grey-600 eds-l-mar-top-1 eds-media-card-content__sub--cropped')[1].text
                 except:
@@ -69,7 +49,6 @@ class EventbriteArt:
                 except:
                     image = 'None'
                 read_more= a_tags[0]['href']
-<<<<<<< HEAD
                 print(read_more)
                 
                 category='FASHION, ART & THEATRE'
@@ -77,27 +56,6 @@ class EventbriteArt:
                     image = 'https://4.bp.blogspot.com/-haQkpIywgPA/W5L1p-6P5JI/AAAAAAAANv4/279R0n1im_MugfsnYTlbf5ZiTaG2s7NYQCLcBGAs/s1600/Six_photoby_IdilSukan_18.jpg'
                 
                 
-=======
-                #print(read_more)
-                # Date formatting
-                
-                date_split = Date_time.split(',')
-                #time = date_split[2]
-                #print(time)
-                date_split_before = date_split[1]
-                date_split_1 = date_split_before.split(' ')
-                date = date_split_1[2]
-                month_before = date_split_1[1]
-                month = datetime.datetime.strptime(month_before,'%b').strftime('%B')
-                year = '2019'
-                Date = date + (' ') + month + (' ')+ year
-            
-                category = 'FASHION, ART & THEATRE'
-                if category == 'FASHION, ART & THEATRE' and image == 'None':
-                    image = 'https://4.bp.blogspot.com/-haQkpIywgPA/W5L1p-6P5JI/AAAAAAAANv4/279R0n1im_MugfsnYTlbf5ZiTaG2s7NYQCLcBGAs/s1600/Six_photoby_IdilSukan_18.jpg'
-            
-                    
->>>>>>> master
                 # description
                 
                 descurl = read_more
@@ -109,7 +67,6 @@ class EventbriteArt:
                 desc_soup = soup(desc_html, "html.parser")
                 #print(desc_soup)
                 
-<<<<<<< HEAD
                 desc = desc_soup.findAll('div', class_='js-xd-read-more-contents l-mar-top-3') or desc_soup.findAll('div',class_='structured-content-rich-text structured-content__module l-align-left l-mar-vert-6 l-sm-mar-vert-4 text-body-medium')
                 if len(desc) > 0:
                     try:
@@ -204,40 +161,6 @@ class EventbriteArt:
                     data.longitude = ordinates[1]                    
                     data_list.append(data)
                     i = i+1
-=======
-                desc = desc_soup.findAll('div', class_='js-xd-read-more-contents l-mar-top-3')
-                if len(desc) == 0:
-                    #print("its empty")
-                    desc = desc_soup.findAll('div', class_= 'structured-content-rich-text structured-content__module l-align-left l-mar-vert-6 l-sm-mar-vert-4 text-body-medium')
-                else:
-                    desc = desc_soup.findAll('div', class_='js-xd-read-more-contents l-mar-top-3')
-                try:
-                    p_tags = desc[0].findAll('p')
-                except:
-                    continue
-                descrip = []
-                for i in range(len(p_tags)):
-                    descript = p_tags[i].text
-                    descrip.append(descript)
-                description = ''.join(str(e) for e in descrip)
-                #print(description)
-
-                data = EventData()
-
-                data.id = uuid.uuid1().__str__()
-                data.title = title
-                data.time = ''
-                data.location = Location
-                data.summary = description
-                data.img = image
-                data.category = category
-                data.startdate = Date
-                data.read_more = read_more
-                data.enddate = ''
-                data.price = Price
-                data_list.append(data)
-                i = i+1
->>>>>>> master
 
             # print(len(data))
 
