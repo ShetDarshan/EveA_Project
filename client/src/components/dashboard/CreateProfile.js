7bc37aa1959c5ac235789c522ff8e3b3eb0b4278
netraj11@gmail.com <sowmya491993@gmail.com>
2019-12-03 08:10:33 +0530

    modal card

--- client/src/components/dashboard/CreateProfile.js
+++ client/src/components/dashboard/CreateProfile.js
@@ -140,6 +140,72 @@ class CreateProfile extends Component {
 
           </div>
 
+          <section>
+            <div className="col-sm-12 col-lg-14">
+
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
+
+{/* <!-- Modal --> */}
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+            <div class="modal-body">
+  <div class="container-fluid">
+    <div class="row">
+      {/* <div class="col-md-4">.col-md-4</div> */}
+      <div class="col-md-4 ml-auto">
+      <h4 className="text-primary">Suggested Friends</h4>
+            <ul className="customFriendList">
+              {
+
+                // sugessted && sugessted.map((e,i )=>{ e.map((f,j)=> {console.log(f.email,"final")})}) 
+
+                sugessted && sugessted.map((data, i) => {
+                  return (data.map((f, j) => {
+                    return (
+
+                      <li key={"profile-" + f} className="card border-primary m-2 p-2">
+                        <Link key={"profile-link-" + f.handle} to={`/friend/${f.email}`}>
+                          <div key={"profile-container-" + f.handle} className="text-center">
+                            <div key={"profile-friendAvtar-" + f.handle} className="friendAvtar m-auto">
+                              <div key={"profile-background-" + f.handle} className="avtarImg" style={{ backgroundImage: `url(${f.imageUrl})` }}></div>
+                            </div>
+                          </div>
+                          <h5 key={"profile-handle-" + f.handle} className="mt-2 text-primary text-center font-weight-bold">{f.handle}</h5>
+                        </Link>
+                      </li>
+                    )
+                  }
+                  ))
+                })
+              }
+              </ul>
+      </div>
+    </div>
+    </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
+
+
+
+
           <div className="w-25">
             <h4 className="text-primary">Requests Received</h4>
             <ul className="friendRequestList ">
