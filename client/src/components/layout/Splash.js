fce16ed1014d851aae945e48f6f91d5ab03c9a8d
netraj11@gmail.com <sowmya491993@gmail.com>
2019-12-04 10:00:55 +0530

    splash fix

--- client/src/components/layout/Splash.js
+++ client/src/components/layout/Splash.js
@@ -7,9 +7,8 @@ import Car1 from '../../img/Carousel.png';
 import Car0 from '../../img/Car2.png';
 import Car3 from '../../img/Car3.png';
 import Car4 from '../../img/Car4.png';
-import { purple } from "@material-ui/core/colors";
-import { PlayCircleFilledWhite, CenterFocusStrong } from "@material-ui/icons";
-import Footer from './Footer'
+import "../../css/splash.css"
+
 
 
  class Splash extends Component {
@@ -19,19 +18,20 @@ import Footer from './Footer'
     
 return (
     
+    <section id="mim">
         <div>  
             
           <Carousel>
             <Carousel.Item >
                 <div><img src={Car1}  alt="Dublin"/></div>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item >
                 <div><img src={Car0}  alt="Signup"/></div>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item >
                 <div><img src={Car3}   alt="Login"/></div>
-            </Carousel.Item>
-            <Carousel.Item>
+            </Carousel.Item >
+            <Carousel.Item >
                 <div><img src={Car4}  alt="Search"/></div>
             </Carousel.Item>
         </Carousel>
@@ -41,7 +41,7 @@ return (
         
 
     </div>   
-                 
+    </section>            
       
 )
 
