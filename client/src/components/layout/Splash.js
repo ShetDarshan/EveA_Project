9b2c5674582fd026123a199ae33e8611ddc59b35
NetraJay <45148429+NetraJay@users.noreply.github.com>
2019-12-04 23:25:03 +0530

    carousel

--- client/src/components/layout/Splash.js
+++ client/src/components/layout/Splash.js
@@ -10,6 +10,7 @@ import Car4 from '../../img/Car4.png';
 import { purple } from "@material-ui/core/colors";
 import { PlayCircleFilledWhite, CenterFocusStrong } from "@material-ui/icons";
 import Footer from './Footer'
+import "../../css/splash.css"
 
 
  class Splash extends Component {
@@ -19,20 +20,21 @@ import Footer from './Footer'
     
 return (
     
-        <div>  
+    <div className="container-fluid banner">
             
           <Carousel>
             <Carousel.Item >
-                <div><a href="./eventboard"><img src={Car1}  alt="Dublin"/></a></div>
+            <div class="d-flex align-items-center justify-content-center min-vh-100">
+                <div><a href="./eventboard"><img src={Car1}  alt="Dublin"/></a></div></div>
             </Carousel.Item>
-            <Carousel.Item>
-                <div><a href="./register"><img src={Car0}  alt="Signup"/></a></div>
+            <Carousel.Item><div class="d-flex align-items-center justify-content-center min-vh-100">
+                <div><a href="./register"><img src={Car0}  alt="Signup"/></a></div></div>
             </Carousel.Item>
-            <Carousel.Item>
-                <div><a href="./login"><img src={Car3}   alt="Login"/></a></div>
+            <Carousel.Item><div class="d-flex align-items-center justify-content-center min-vh-100">
+                <div><a href="./login"><img src={Car3}   alt="Login"/></a></div></div>
             </Carousel.Item>
-            <Carousel.Item>
-                <div><a href="./eventboard"><img src={Car4}  alt="Search"/></a></div>
+            <Carousel.Item><div class="d-flex align-items-center justify-content-center min-vh-100">
+                <div><a href="./eventboard"><img src={Car4}  alt="Search"/></a></div></div>
             </Carousel.Item>
         </Carousel>
     
